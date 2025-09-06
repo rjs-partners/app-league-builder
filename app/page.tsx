a263d27325b73aebd0288b659e86cf2fc20a484a
rjs-partners <robsmallcombe@gmail.com>
2025-09-06 16:30:50 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import LeagueBuilderPrototype from '../components/LeagueBuilderPrototype';
+import LeagueBuilderPrototype from '@/components/LeagueBuilderPrototype';
 
-export default function Page() {
-  return <LeagueBuilderPrototype/>;
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-slate-50 p-6">
+      <LeagueBuilderPrototype />
+    </main>
+  );
 }
